a1eaf752c1a59d0257c6bbf20b79ae676ca5d884
SJ <russiansky912@gmail.com>
2024-12-07 20:14:44 +0000

    Add files via upload

---
+++ labs/lab08/lab8-1-2.asm
@@ -0,0 +1,35 @@
+%include 'in_out.asm'
+
+SECTION .data
+msg1 db 'Введить N:',0h
+SECTION .bss
+    N: resb 10
+
+SECTION .text
+   Global _start
+_start:
+
+;------ Вывод сообщения 'Введите N:'
+mov eax,msg1
+call sprint
+;----- Вывод 'N'
+mov ecx,N
+mov edx, 10
+call sread
+
+; Преобразования 'N' из символа в число
+mov eax,N
+call atoi
+mov [N],eax
+
+;------ Организация цикла
+mov ecx,[N]
+label :
+push ecx
+sub ecx,1
+mov [N],ecx
+mov eax,[N]
+call iprintLF
+pop ecx
+loop label
+call quit
